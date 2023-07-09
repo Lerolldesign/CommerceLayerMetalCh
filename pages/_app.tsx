7ed9331426e0ca115ef66202c80b57adde2e77eb
Lerolldesign <iam@lerolldesign.com>
2023-07-09 14:03:24 +0200

    double quote

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,23 +3,21 @@ import { AppProps } from "next/app";
 import "../styles/main.css";
 import "../styles/app.css";
 
-import { Inter } from 'next/font/google';
+import { Inter } from "next/font/google";
 import localFont from "next/font/local";
  
 const sans = Inter({
-  subsets: ['latin'],
+  subsets: ['latin'] ,
   weight: ['200', '300','500','600', '700', '800'],
   display: 'swap',
   variable: '--font-sans',
 });
  
 const metalch = localFont({
-  src:'../fonts/integralcf-bold.ttf',
+  src: '../fonts/integralcf-bold.ttf',
   display: 'swap',
   variable: '--font-metalch',
 });
-
-
 
 function CustomApp({ Component, pageProps }: AppProps) {
   return (
